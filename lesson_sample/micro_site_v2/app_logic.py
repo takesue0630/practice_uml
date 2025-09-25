0e62cc227a658fb691eec1def31901b0d1c84085
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-09-25 11:42:09 +0900

    【追記】足し算の数値セット

--- lesson_sample/micro_site_v2/app_logic.py
+++ lesson_sample/micro_site_v2/app_logic.py
@@ -45,6 +45,18 @@ def get_my_greeting() -> str:
     except (json.JSONDecodeError, OSError):
         return ""
 
-# set_first_value: first_valueをjsonに保存
-# set_second_value: second_valueをjsonに保存
-# get_addition: jsonから値を取ってきて、足し算した結果を返却
+def set_first_value(first_value) -> None:
+    """一つ目の値をJSONに保存"""
+    with open(ROBODOG_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    data['my_name'] = first_value
+    with open(ROBODOG_FILE, 'w', encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def set_second_value(second_value) -> None:
+    """一つ目の値をJSONに保存"""
+    with open(ROBODOG_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    data['my_name'] = second_value
+    with open(ROBODOG_FILE, 'w', encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
